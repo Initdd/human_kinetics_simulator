e592a4aa1b05f5a9fe4868d52a4af2e061b9fa56
Initdd <a50468@alunos.isel.pt>
2024-04-20 18:23:01 +0100

    separated the convert to positive angle function to the point one

--- utils/point.py
+++ utils/point.py
@@ -2,6 +2,7 @@ import math
 from typing import Tuple
 
 DEC_VAL = 5
+ANGLE_MAX = 360
 
 
 def rotate_point(point: Tuple[float, float], center: Tuple[float, float], angle: float) -> Tuple[float, float]:
@@ -11,3 +12,15 @@ def rotate_point(point: Tuple[float, float], center: Tuple[float, float], angle:
 	new_x: float = round(x * math.cos(angle_rad) - y * math.sin(angle_rad), DEC_VAL)
 	new_y: float = round(x * math.sin(angle_rad) + y * math.cos(angle_rad), DEC_VAL)
 	return (new_x + center[0], new_y + center[1])
+
+def convert_to_positive_angle(angle: int) -> int:
+	"""
+		Function to convert an angle to a positive angle
+
+		Args:
+			angle: The angle to convert
+
+		Returns:
+			The positive angle
+	"""
+	return (angle + ANGLE_MAX) % ANGLE_MAX
